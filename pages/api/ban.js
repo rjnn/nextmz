df00c9704b2fe14fa6ac247a5728156a8b1a8299
Arjun Narayan <arjun@materialize.io>
2022-08-21 17:49:59 -0400

    factor out result

--- pages/api/ban.js
+++ pages/api/ban.js
@@ -4,16 +4,18 @@ import { mzpool } from '../../lib/mz';
 var v = 1;
 
 export default async function ban(req, res) {
+  var mzr = -1;
   mzpool
     .query('SELECT $1', [v])
     .then(r => {
-      console.log(r.rows[0]);
+      mzr = r.rows[0]
+      console.log(mzr);
       v++
     })
     .catch(e => {
       console.error(e.stack)
     })
 
-  res.status(200).json(0);
+  res.status(200).json(mzr);
   return;
 }
