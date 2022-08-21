f61b74e17de033e604ccd0c60ac2ae99913636c4
Arjun Narayan <arjun@materialize.io>
2022-08-21 17:44:58 -0400

    move mz code to lib

--- pages/api/ban.js
+++ pages/api/ban.js
@@ -1,26 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { mzpool } from '../../lib/mz';
 
-const { Pool } = require("pg");
-
-const mz_sql_endpoint = process.env.MZ_SQL_ENDPOINT;
-const mz_user = process.env.MZ_USER;
-const mz_password = process.env.MZ_PASSWORD;
-const mz_db = process.env.MZ_DATABASE;
-
-const pool = new Pool({
-  host: mz_sql_endpoint,
-  port: 6875,
-  user: mz_user,
-  password: mz_password,
-  database: mz_db,
-  ssl: true
-});
-
-const testQuery = "SELECT 17";
 var v = 1;
 
 export default async function ban(req, res) {
-  pool
+  mzpool
     .query('SELECT $1', [v])
     .then(r => {
       console.log(r.rows[0]);
@@ -30,6 +14,6 @@ export default async function ban(req, res) {
       console.error(e.stack)
     })
 
-  res.status(200).json();
+  res.status(200).json(0);
   return;
 }
